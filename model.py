0e98295cc84f0790668942c188b4841c59f7a23b
unknown <delphinel@gmail.com>
2015-02-09 16:09:47 +0100

    svm

--- model.py
+++ model.py
@@ -1,18 +1,15 @@
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import SelectPercentile, f_classif
-from sklearn import svm
+from sklearn.ensemble import GradientBoostingClassifier
 
 def model(X_train, y_train, X_test):
-    weight={1.0:2, 0.0:1}
     clf = Pipeline([('imputer', Imputer()),
                 ('scaler', StandardScaler()),
                 ('select', SelectPercentile(f_classif, 90)),
-                ('svm', svm.SVC(C=1.0, cache_size=200, class_weight=weight, coef0=0.0, degree=3,
-                  gamma=0.0, kernel='rbf', max_iter=-1, probability=True, random_state=None,
-                  shrinking=True, tol=0.001, verbose=False))])
+                ('clf', GradientBoostingClassifier(n_estimators=200, max_leaf_nodes=4, max_depth=None, random_state=2,
+                   min_samples_split=5, subsample=0.5))])
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
