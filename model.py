3763b9add3b18c858ece9c0328385cc00b3b870d
unknown <delphinel@gmail.com>
2015-02-09 14:42:11 +0100

    svm

--- model.py
+++ model.py
@@ -3,8 +3,10 @@ from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 
 def model(X_train, y_train, X_test):
-    clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-        ('rf', RandomForestClassifier(n_estimators=25))])
+    clf = Pipeline([('imputer', Imputer()),
+                ('scaler', StandardScaler()),
+                ('select', SelectPercentile(f_classif, 90)),
+                ('svm', svm.SVC())])
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
